9fa4d24c69c9a41f4f2e3b9040f1e807d978951b
JNettli <jonas.nettli@gmail.com>
2024-11-07 13:20:24 +0100

    feat: added fake logo to project and start of feed page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,9 @@ module.exports = {
   content: ["./**/*.{html,js}", "./!node_modules"],
   theme: {
     extend: {
-      colors: {
-        'primary': '#FF6363',
-        'secondary': {
-          100: '#E2E2D5',
-          200: '#888883',
-        }
-      }
+      fontFamily: {
+        custom: ["Nunito"],
+      },
     },
   },
   plugins: [],
